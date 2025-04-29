3ff77a241c4342f426ec9acb937ddebdc33efb95
rafael-staine <rafael.staine@aluno.ifsp.edu.br>
2025-04-29 17:39:33 -0300

    Adicionando barra de pesquisa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Dropdown
 document.addEventListener('DOMContentLoaded', function() {
     const dropdownToggle = document.querySelectorAll('[data-dropdown-toggle]');
     dropdownToggle.forEach((toggle) => {
@@ -7,4 +8,38 @@ document.addEventListener('DOMContentLoaded', function() {
             parent.classList.toggle('active');
         })
     })
+    
+    // Fechar o dropdown ao clicar fora
+    document.addEventListener('click', function (e) {
+        dropdownToggle.forEach((toggle) => {
+            const parent = toggle.closest('.dropdown-item');
+            if (!parent.contains(e.target)) {
+                parent.classList.remove('active');
+            } 
+        })
+    })
+})
+
+// barra de pesquisa
+const searchToggle = document.getElementById('searchToggle');
+const searcOverlay = document.getElementById('searchOverlay');
+const closeSearch = document.getElementById('closeSearch');
+
+searchToggle.addEventListener('click', () => {
+    searcOverlay.style.display = 'flex';
+    setTimeout(() => {
+        searcOverlay.querySelector('.search-input').focus();
+    }, 100);
+})
+
+// fechar o botão
+closeSearch.addEventListener('click', () => {
+    searcOverlay.style.display = 'none';
+})
+
+// fechar com o ESC
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        searcOverlay.style.display = 'none';
+    }
 })
